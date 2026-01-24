ddc5047a5e6c15f5b06b68d5b254b24a1169074f
Francis Anyuru <anyuruf@gmail.com>
2026-01-23 20:25:20 -0500

    reset entry files

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,11 +1,12 @@
-import { startTransition } from 'react'
-import { hydrateRoot } from 'react-dom/client'
-import { HydratedRouter } from 'react-router/dom'
-
-if (ENV.MODE === 'production' && ENV.SENTRY_DSN) {
-	void import('./utils/monitoring.client.tsx').then(({ init }) => init())
-}
+import { startTransition, StrictMode } from "react";
+import { hydrateRoot } from "react-dom/client";
+import { HydratedRouter } from "react-router/dom";
 
 startTransition(() => {
-	hydrateRoot(document, <HydratedRouter />)
-})
+  hydrateRoot(
+    document,
+    <StrictMode>
+      <HydratedRouter />
+    </StrictMode>,
+  );
+});
